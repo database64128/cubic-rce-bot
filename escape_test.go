5efe01dcaff7f4ddae495ac2176e16d43e8ebb02
database64128 <free122448@hotmail.com>
2025-06-17 15:24:57 +0800

    🥕 rcebot: clean up tests

--- escape_test.go
+++ escape_test.go
@@ -1,52 +1,52 @@
-package rcebot
+package rcebot_test
 
 import (
 	"errors"
 	"testing"
+
+	rcebot "github.com/database64128/cubic-rce-bot"
 )
-
-const (
-	outputEscape   = "`\\\\localhost`\n"
-	responseEscape = "```\n\\`\\\\\\\\localhost\\`\n```\n"
-
-	outputNoEscape   = "localhost\n"
-	responseNoEscape = "```\nlocalhost\n```\n"
-
-	outputNoLF   = "localhost"
-	responseNoLF = responseNoEscape
-
-	outputMultiLine   = "localhost\nlocalhost\n"
-	responseMultiLine = "```\nlocalhost\nlocalhost\n```\n"
-
-	errString         = "1 + 1 = 2"
-	responseEscapeErr = responseEscape + "1 \\+ 1 \\= 2"
-)
-
-var errTest = errors.New(errString)
-
-func testCommandOutputResponseBuilder(t *testing.T, rb *CommandOutputResponseBuilder, output []byte, err error, expectedResponse string) {
-	t.Helper()
-	response := rb.Build(output, err)
-	if response != expectedResponse {
-		t.Errorf("expected response %q, got %q", expectedResponse, response)
-	}
-}
 
 func TestCommandOutputResponseBuilder(t *testing.T) {
-	rb := CommandOutputResponseBuilder{}
-	t.Run("Escape", func(t *testing.T) {
-		testCommandOutputResponseBuilder(t, &rb, []byte(outputEscape), nil, responseEscape)
-	})
-	t.Run("NoEscape", func(t *testing.T) {
-		testCommandOutputResponseBuilder(t, &rb, []byte(outputNoEscape), nil, responseNoEscape)
-	})
-	t.Run("NoLF", func(t *testing.T) {
-		testCommandOutputResponseBuilder(t, &rb, []byte(outputNoLF), nil, responseNoLF)
-	})
-	t.Run("MultiLine", func(t *testing.T) {
-		testCommandOutputResponseBuilder(t, &rb, []byte(outputMultiLine), nil, responseMultiLine)
-	})
-	t.Run("EscapeErr", func(t *testing.T) {
-		testCommandOutputResponseBuilder(t, &rb, []byte(outputEscape), errTest, responseEscapeErr)
-	})
+	rb := rcebot.CommandOutputResponseBuilder{}
+
+	for _, c := range [...]struct {
+		name   string
+		output string
+		err    error
+		want   string
+	}{
+		{
+			name:   "Escape",
+			output: "`\\\\localhost`\n",
+			want:   "```\n\\`\\\\\\\\localhost\\`\n```\n",
+		},
+		{
+			name:   "NoEscape",
+			output: "localhost\n",
+			want:   "```\nlocalhost\n```\n",
+		},
+		{
+			name:   "NoLF",
+			output: "localhost",
+			want:   "```\nlocalhost\n```\n",
+		},
+		{
+			name:   "MultiLine",
+			output: "localhost\nlocalhost\n",
+			want:   "```\nlocalhost\nlocalhost\n```\n",
+		},
+		{
+			name:   "EscapeErr",
+			output: "`\\\\localhost`\n",
+			err:    errors.New("1 + 1 = 2"),
+			want:   "```\n\\`\\\\\\\\localhost\\`\n```\n1 \\+ 1 \\= 2",
+		},
+	} {
+		t.Run(c.name, func(t *testing.T) {
+			if got := rb.Build([]byte(c.output), c.err); got != c.want {
+				t.Errorf("rb.Build(%q, %v) = %q, want %q", c.output, c.err, got, c.want)
+			}
+		})
+	}
 }
